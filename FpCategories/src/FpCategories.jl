cfa3c36c4084e2a07357205062cea789bbbdde64
Kamal Saleh <kamal.saleh@web.de>
2025-08-13 14:36:19 +0200

    Transpile FpCategories v2025.08-01
    Current Version to v0.0.1

---
+++ FpCategories/src/FpCategories.jl
@@ -0,0 +1,17 @@
+module FpCategories
+
+@nospecialize
+
+using CAP
+
+using CartesianCategories
+
+using ToolsForCategoricalTowers
+
+using QuotientCategories
+
+using FinSetsForCAP
+
+include("init.jl")
+
+end # module FpCategories
