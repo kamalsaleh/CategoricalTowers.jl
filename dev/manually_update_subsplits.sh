ea07fd4b4ab3f83a7d599e1a5e0a10f3b5923b52
Kamal Saleh <kamal.saleh@web.de>
2025-08-22 12:48:26 +0200

    add makefile and /dev and ci-tests

---
+++ dev/manually_update_subsplits.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+# Define GitHub username
+if [ -z "$1" ]; then
+  GITHUB_USER="homalg-project"
+else
+  GITHUB_USER="$1"
+fi
+
+# update git index
+git status > /dev/null
+
+if ! git diff-index --quiet HEAD; then
+	echo -e "\033[33mWARNING: Dirty working tree.\033[0m"
+fi
+
+if [[ "$(git rev-parse --abbrev-ref HEAD)" != "master" ]]; then
+	echo -e "\033[31mERROR: Not on branch master\033[0m"
+	exit 1
+fi
+
+# ToolsForCategoricalTowers
+git subtree split --prefix=ToolsForCategoricalTowers -b ToolsForCategoricalTowers-split
+git push git@github.com:${GITHUB_USER}/ToolsForCategoricalTowers.jl.git ToolsForCategoricalTowers-split:master
+echo "Pushed to ToolsForCategoricalTowers.jl"
+
+# QuotientCategories
+git subtree split --prefix=QuotientCategories -b QuotientCategories-split
+git push git@github.com:${GITHUB_USER}/QuotientCategories.jl.git QuotientCategories-split:master
+echo "Pushed to QuotientCategories.jl"
---
+++ dev/update_subsplits.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+# Check if token exists
+if [ -z "$JULIA_SUBSPLIT_TOKEN" ]; then
+  echo -e "\033[31mERROR: Missing JULIA_SUBSPLIT_TOKEN\033[0m"
+  exit 1
+fi
+
+# update git index
+git status > /dev/null
+
+if ! git diff-index --quiet HEAD; then
+	echo -e "\033[33mWARNING: Dirty working tree.\033[0m"
+fi
+
+if [[ "$(git rev-parse --abbrev-ref HEAD)" != "master" ]]; then
+	echo -e "\033[31mERROR: Not on branch master\033[0m"
+	exit 1
+fi
+
+# ToolsForCategoricalTowers
+git subtree split --prefix=ToolsForCategoricalTowers -b ToolsForCategoricalTowers-split > /dev/null 2>&1
+git push https://${JULIA_SUBSPLIT_TOKEN}@github.com/homalg-project/ToolsForCategoricalTowers.jl.git ToolsForCategoricalTowers-split:master
+echo "Pushed to ToolsForCategoricalTowers.jl"
+
+# QuotientCategories
+git subtree split --prefix=QuotientCategories -b QuotientCategories-split > /dev/null 2>&1
+git push https://${JULIA_SUBSPLIT_TOKEN}@github.com/homalg-project/QuotientCategories.jl.git QuotientCategories-split:master
+echo "Pushed to QuotientCategories.jl"
