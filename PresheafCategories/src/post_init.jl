676227b58a8302fb1632611466a4d5ca62312980
Kamal Saleh <kamal.saleh@web.de>
2026-09-01 12:40:34 +0200

    PresheafCategories: now CAP.jl supports calling functors and natural transformations
    Bump PresheafCategories to v0.1.6 (requires CAP v0.8.17)

--- PresheafCategories/src/post_init.jl
+++ PresheafCategories/src/post_init.jl
@@ -6,12 +6,3 @@ function (F::IsMorphismInPreSheafCategory.abstract_type)(c::IsCapCategoryObject.
     CallFuncList(F, [c])
 end
 
-# NEVER COMMIT THIS: MOVE TO CAP.jl (or ToolsForCategoricalTowers.jl where CallFuncList is installed)
-function (F::IsCapFunctor.abstract_type)(c::IsCapCategoryCell.abstract_type)
-    CallFuncList(F, [c])
-end
-
-function (F::IsCapNaturalTransformation.abstract_type)(c::IsCapCategoryObject.abstract_type)
-    CallFuncList(F, [c])
-end
-###################################
